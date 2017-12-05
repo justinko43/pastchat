1076c219af2e6ea3a535f6a9845d37b56a0726a4
mikeg <mikeg@steelhouse.com>
2017-12-05 11:21:13 -0800

    add styleguide & less files

--- client/index.js
+++ client/index.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App.jsx';
 
+// Styles
+import './styles/app.less'
+
 render(
     <App />,
     document.getElementById('root')
