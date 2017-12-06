775e71ffa32e8fb2def2726adc30e696c7158c28
mikeg <mikeg@steelhouse.com>
2017-12-06 15:41:28 -0800

    style summary title

--- client/components/Main/Summary.jsx
+++ client/components/Main/Summary.jsx
@@ -6,7 +6,7 @@ class Summary extends Component {
     render() {
         return (
             <div>
-                <h3>{this.props.title}</h3>
+                <h3 className="fw-600">{this.props.title}</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem perferendis quisquam asperiores error atque accusantium tempore, dolores consequatur? Distinctio error tempora libero deleniti nulla quaerat accusamus quidem. Beatae, provident.</p>
             </div> 
         )
