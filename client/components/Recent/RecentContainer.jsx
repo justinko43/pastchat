c6506a735bd916ad994a1db0cb9e97a35f1e3d1e
mikeg <mikeg@steelhouse.com>
2017-12-06 13:45:35 -0800

    recent placeholders

--- client/components/Recent/RecentContainer.jsx
+++ client/components/Recent/RecentContainer.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import RecentVid from './RecentVid';
+import placeholders from './placeholders.js'
 
 
 class Recent extends Component {
+    constructor(props) {
+        super(props);
+        this.renderPlaceholders = this.renderPlaceholders.bind(this);
+    }
+
+    renderPlaceholders() {
+        return placeholders.map((placeholder, idx) => {
+            return (
+                <RecentVid key={idx} title={placeholder.title} src={placeholder.src} channel={placeholder.channel} />
+            );
+        });
+    }
+
     render() {
         return (
             <div id="recent-container" className="bg-gr3">
-                <h3 id="recent-video-title">Recent Videos</h3>
-                <RecentVid/>
+                <div className="fw-600 h4 margin-top margin-bottom text-center">Recent Videos</div>
+                { this.renderPlaceholders() }
             </div> 
         )
     }
---
+++ client/components/Recent/placeholders.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    title: 'iD3',
+    src: 'https://i.ytimg.com/vi/jioJ16n4THU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCnsKjs0APfRp3x_uKksNANSyQ5bA',
+    channel: 'Codesmith',
+  },
+  {
+    title: 'React-rpm',
+    src: 'https://i.ytimg.com/vi/542OJRZ3Ses/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLD70EQpEFQRZxS_QdEeOeHStsyGdw',
+    channel: 'Codesmith',
+  },
+  {
+    title: 'Nile.js',
+    src: 'https://i.ytimg.com/vi/HceKxuYrkxM/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLB5GUxZTdHLtRbLwqNOvgR7R174Yw',
+    channel: 'Codesmith',
+  },
+  {
+    title: 'Expressive.js',
+    src: 'https://i.ytimg.com/vi/mKGjoCgtXQk/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDBsMqHf5TQjHvKv-wQcNXV73QQAA',
+    channel: 'Codesmith',
+  },
+  {
+    title: 'Javascript the Hard Parts - Closure, Scope, and Execution Context (11/17)',
+    src: 'https://i.ytimg.com/vi/ILfco5UTkr8/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAbrPFXfhYbZvtztPrgoHB9MujXGQ',
+    channel: 'Codesmith',
+  },
+  {
+    title: 'Rhythm Roulette: 9th Wonder',
+    src: 'https://i.ytimg.com/vi/Ce8r9RfK1wk/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAxm1TaDjBuF5jh6r8ZighAApmWmQ',
+    channel: 'Mass Appeal',
+  },
+  {
+    title: 'Rhythm Roulette: Mac Miller aka Larry Fisherman',
+    src: 'https://i.ytimg.com/vi/X-R47DwZWkU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCzRRpUcs-KHYcOE3X2h7zAQDrbrw',
+    channel: 'Mass Appeal',
+  },
+  {
+    title: 'BASE jumping into a plane mid-air. (Extended Version) | A Door In The Sky',
+    src: 'https://i.ytimg.com/vi/Qh06wlg9RmU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC8eTdS0tUMJYOI3s09IuYTjxTzHg',
+    channel: 'Red Bull',
+  },
+  {
+    title: '40 athletes, 20 switchbacks, 4 kilometers. This is Red Bull Trike Strike.',
+    src: 'https://i.ytimg.com/vi/tSe0RvFsOZU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDeA5XOlQRX3O6KP6gwGZIRDaHc8g',
+    channel: 'Red Bull',
+  },
+  {
+    title: 'Episode 1 of 7 | Hurley Presents "Twelve": A New Series From John John Florence',
+    src: 'https://i.ytimg.com/vi/uJETuRcG79w/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAOKhUMRJjTFMdxJ5vVS-PBUqGjcQ',
+    channel: 'Hurley',
+  },
+]
