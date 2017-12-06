38f9ddd764c2fe3de29986c3a987eb15c0c92f90
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2017-12-05 17:21:31 -0800

    created getComments controller to be used with server route

--- server/database/post-controller.js
+++ server/database/post-controller.js
@@ -1,8 +1,26 @@
 const db = require('./postgres.js');
 
-db.connect(async () => {
-  await db.query('INSERT INTO posts (columntest) values (\'test\')');
-  const result = await db.query('SELECT * FROM posts;');
-  console.log(result.rows);
-  db.end();
-});
+const controller = {
+  getComments: async function(req, res, next) {
+    db.connect(async () => {
+      const videoId = req.params.videoId;
+      const results = await db.query(`SELECT (c.comment, c.timestamp, u.name, u.avatar) FROM comments c INNER JOIN users u ON (c.userid = u.userid) WHERE c.video_id = ('${videoId}');`);
+      const finalArray = [];
+      results.rows.forEach(row => {
+        const commentObj = {};
+        const arr = row.row.split(',');
+        arr[0] = arr[0].split('').filter((c, i, a) => i > 1 && i < (a.length - 1)).join('');
+        arr[3] = arr[3].split('').filter((c, i, a) => i < (a.length - 1)).join('');
+        commentObj.message = arr[0];
+        commentObj.timestamp = arr[1];
+        commentObj.name = arr[2];
+        commentObj.avatar = arr[3];
+        finalArray.push(commentObj);
+      });
+      res.locals.comments = finalArray;
+      db.end();
+    });
+  }
+}
+
+module.exports = controller;
