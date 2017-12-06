43f1bb23c38485071ae296441f0e7d87bbcb477a
justin.ko43 <justin.ko43@gmail.com>
2017-12-06 12:30:46 -0800

    updated master

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleAuth : {
+    clientID      : '371110817153-6bcet3rvgje9v582itfpgh6rl7p7i4fb.apps.googleusercontent.com',
+    clientSecret  : 'unqLNF6fifnloBDOnypcmrkX',
+    callbackURL   : 'http://localhost:3000/auth/google/callback'
+  }
+};
